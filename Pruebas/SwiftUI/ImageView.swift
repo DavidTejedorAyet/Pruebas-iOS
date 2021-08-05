23191ece234f4b3b1222513691fb49e534c474c5
David Tejedor <david.tejedor@outlook.com>
2021-08-05 11:13:24 +0200

    Image view

---
+++ Pruebas/SwiftUI/ImageView.swift
@@ -0,0 +1,24 @@
+//
+//  ImageView.swift
+//  Pruebas
+//
+//  Created by David on 4/8/21.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 30) {
+            Image("michi").resizable().padding(80).background(Color.gray).scaledToFill().frame(width: 300, height: 300).clipShape(Circle()).overlay(Circle().stroke(Color.blue, lineWidth: 9)).shadow(radius: 10)
+            
+            Image(systemName: "person.fill.badge.minus").resizable().padding(80).background(Color.gray).scaledToFill().frame(width: 300, height: 300).clipShape(Circle()).overlay(Circle().stroke(Color.blue, lineWidth: 9)).shadow(radius: 10)
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
